95bbf82d41350ff46261d0875e84839fa26c059c
田 知勲 <j-jihoon@winas.jp>
2021-11-23 00:42:11 +0900

    초기 설정

---
+++ NeflixStyleView/NeflixStyleView/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  NeflixStyleView
+//
+//  Created by 전지훈 on 2021/11/23.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
